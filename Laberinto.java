9f697bd86a09ed07100358dff6192fc790c0cf27
lumoreira <lulii.moreira96@gmail.com>
2026-05-10 11:41:24 -0300

    corrijo puntos cardinales

--- Laberinto.java
+++ Laberinto.java
@@ -65,14 +65,14 @@ public class Laberinto {
             }
         }
     
-        if(mat[fi][ci].isOeste() && ci>mat[0].length && !visitados[fi][ci-1]){
+        if(mat[fi][ci].isOeste() && ci>0 && !visitados[fi][ci-1]){
             int res = buscarCaminoMenorLongitud(mat, fi, ci-1, ff, cf, visitados);
             if(res!=Integer.MAX_VALUE){
                 minRestante=Math.min(minRestante, res);
             }
         }
     
-        if(mat[fi][ci].isEste() && ci<0 && !visitados[fi][ci+1]){
+        if(mat[fi][ci].isEste() && ci<mat[0].length && !visitados[fi][ci+1]){
             int res = buscarCaminoMenorLongitud(mat, fi, ci+1, ff, cf, visitados);
             if(res!=Integer.MAX_VALUE){
                 minRestante=Math.min(minRestante, res);
